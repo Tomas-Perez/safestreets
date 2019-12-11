202d4fa06ccb0d4d9c64257633d98972f9e7f58d
lethanity <manupedr@gmail.com>
2019-12-11 21:17:16 +0200

    backend init

---
+++ implementation/back/src/main/kotlin/se2/SafeStreets/back/controller/example.kt
@@ -0,0 +1,14 @@
+package se2.SafeStreets.back.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class ArticleController {
+
+    @GetMapping("/test")
+    fun getTest(): List<String> =
+            listOf("hello", "there")
+}
